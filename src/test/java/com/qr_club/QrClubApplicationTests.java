15b3cb0e47e6b0698b46a1c8403d71e23d5678eb
gkwgu <polina.petr0v4@yandex.ru>
2026-01-30 17:13:16 +0300

    First commit

---
+++ src/test/java/com/qr_club/QrClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qr_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
